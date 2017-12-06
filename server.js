ce5421045325f7ca22ce20525c47010d989cd92a
rxguadalupe <rxguadalup3@hotmail.com>
2017-12-06 19:24:45 -0400

    package.json

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fecha = new Date();
+var port = process.env.port || 3000
 
 var server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-type': 'text/html'});	
+	res.writeHead(200, {'Content-type': 'application/JSON'});
 	res.write(fecha.getFullYear().toString());
 	res.end();
 });
