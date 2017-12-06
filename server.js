9d4ef6654fb15cb6bae31bc482fe1d9257e18e24
Norman Trujillo <natrujillo9@gmail.com>
2017-12-06 19:28:57 -0400

    Require URL con if y else.
    Si el usuario escribe localhost:8080/super-chat entrara el HTTP
    request, pero si escribe cualquier otro path le saldrá un 404 error.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require('http');
+var url  = require('url');
 var fecha = new Date();
 var port = process.env.PORT || 3000;
 
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-type': 'application/JSON'});
 	res.write(fecha.getFullYear().toString());
+	if (req.url === '/super-chat') {
+	} else {
+		res.writeHead(404);
+	}
 	res.end();
 });
 
