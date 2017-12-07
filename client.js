303bce00b18de10bb64e5343886091d6ce8bc888
Bryan Collazo <bcollazo2010@gmail.com>
2017-12-06 20:06:18 -0400

    Make server collect messages, make client send messages and print results.

--- client.js
+++ client.js
@@ -1,9 +1,20 @@
 const prompt = require("prompt-sync")();
+const request = require("sync-request");
+
+const url = process.argv[2] + '/super-chat';
 
 var nombre = prompt("Escribe tu nombre: ");
 
 while (chat !== "") {
   var chat = prompt(">>> ");
+  var res = request('POST', url, {
+    json: {
+      name: nombre,
+      message: chat
+    }
+  });
+  var messages = JSON.parse(res.getBody('utf8'));
+  console.log(messages);
 }
 
 console.log("Ready" + "🙌 ");
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var http = require('http');
-var fecha = new Date();
 var port = process.env.PORT || 3000;
 
+var messages = [];
 var server = http.createServer(function(req, res) {
 	if (req.url === '/super-chat') {
-		res.writeHead(200, {'Content-type': 'application/JSON'});
-		res.write(fecha.getFullYear().toString());
+		var body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        message = JSON.parse(Buffer.concat(body).toString());
+        messages.push(message);
+
+        res.writeHeader(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({
+            'messages': messages
+        }));
+        res.end();
+    });
 	} else {
 		res.writeHead(404);
+		res.end();
 	}
-	res.end();
 });
 
 server.listen(port);
